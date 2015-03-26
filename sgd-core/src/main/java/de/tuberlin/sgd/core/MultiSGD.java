2b7948ccb58a9259cacc5f0fb8dde9571da6597e
elbehery <eng.m.behery@live.com>
2015-03-26 19:50:31 +0100

    Buzz DataSet

--- sgd-core/src/main/java/de/tuberlin/sgd/core/MultiSGD.java
+++ sgd-core/src/main/java/de/tuberlin/sgd/core/MultiSGD.java
@@ -21,26 +21,36 @@ public class MultiSGD {
 
         for (int x = 0; x < numOfIteration; x++) {
 
-            params[x] = new DVector(58);
+            params[x] = new DVector(97);
             final int nThreads = 5;
 
             //final String[] fields = new String[]{"sepal-length", "sepal-width", "petal-length", "petal-width", "label"};
-            final String[] spamFields = new String[]{"word_freq_make", "word_freq_address", "word_freq_all", "word_freq_3d", "word_freq_our", "word_freq_over", "word_freq_remove", "word_freq_internet", "word_freq_order", "word_freq_mail", "word_freq_receive",
-                    "word_freq_will", "word_freq_people", "word_freq_report", "word_freq_addresses", "word_freq_free", "word_freq_business", "word_freq_email", "word_freq_you", "word_freq_credit",
-                    "word_freq_your", "word_freq_font", "word_freq_000", "word_freq_money", "word_freq_hp", "word_freq_hpl", "word_freq_george", "word_freq_650", "word_freq_lab", "word_freq_labs", "word_freq_telnet", "word_freq_857",
-                    "word_freq_data", "word_freq_415", "word_freq_85", "word_freq_technology", "word_freq_1999", "word_freq_parts", "word_freq_pm", "word_freq_direct", "word_freq_cs", "word_freq_meeting", "word_freq_original", "word_freq_project",
-                    "word_freq_re", "word_freq_edu", "word_freq_table", "word_freq_conference", "char_freq_;", "char_freq_(", "char_freq_[", "char_freq_!", "char_freq_$", "char_freq_#", "capital_run_length_average", "capital_run_length_longest", "capital_run_length_total", "Label"};
+
+            final String[] buzzFields= new String[]{"NCD_0","NCD_1","NCD_2","NCD_3","NCD_4","NCD_5","NCD_6","NCD_7",
+                                                     "BL_0","BL_1","BL_2","BL_3","BL_4","BL_5","BL_6","BL_7",
+                                                     "NAD_0","NAD_1","NAD_2","NAD_3","NAD_4","NAD_5","NAD_6","NAD_7",
+                                                     "AI_0","AI_1","AI_2","AI_3","AI_4","AI_5","AI_6","AI_7",
+                                                     "NAC_0","NAC_1","NAC_2","NAC_3","NAC_4","NAC_5","NAC_6","NAC_7",
+                                                     "ND_0","ND_1","ND_2","ND_3","ND_4","ND_5","ND_6","ND_7",
+                                                     "CS_0","CS_1","CS_2","CS_3","CS_4","CS_5","CS_6","CS_7",
+                                                     "AT_0","AT_1","AT_2","AT_3","AT_4","AT_5","AT_6","AT_7",
+                                                     "NA_0","NA_1","NA_2","NA_3","NA_4","NA_5","NA_6","NA_7",
+                                                     "ADL_0","ADL_1","ADL_2","ADL_3","ADL_4","ADL_5","ADL_6","ADL_7",
+                                                     "AS(NA)_0","AS(NA)_1","AS(NA)_2","AS(NA)_3","AS(NA)_4","AS(NA)_5","AS(NA)_6","AS(NA)_7",
+                                                     "AS(NAC)_0","AS(NAC)_1","AS(NAC)_2","AS(NAC)_3","AS(NAC)_4","AS(NAC)_5","AS(NAC)_6","AS(NAC)_7",
+                                                     "prediction"};
+
 
             //DVectorFrame frame = CSVDataReader.readCSV("ML_Data/iris/iris.data", spamFields);
-            DVectorFrame frame = CSVDataReader.readCSV("ML_Data/Spambase/spambase.data", spamFields);
-            List<DVectorFrame> splittedList = frame.split(nThreads, spamFields);
+            DVectorFrame frame = CSVDataReader.readCSV("ML_Data/Buzz/TomsHardware.data", buzzFields);
+            List<DVectorFrame> splittedList = frame.split(nThreads, buzzFields);
 
             ExecutorService executorService = Executors.newFixedThreadPool(nThreads);
-            int[] X_indices = Ints.toArray(ContiguousSet.create(Range.closed(0, 56), DiscreteDomain.integers()));
+            int[] X_indices = Ints.toArray(ContiguousSet.create(Range.closed(0, 95), DiscreteDomain.integers()));
 
             for (int i = 0; i < nThreads; i++) {
                 System.out.println("Thread No "+i);
-                executorService.execute(new RunnableSGD(params[x], splittedList.get(0), X_indices, 57, splittedList.get(0).getVec(0).elements.length));
+                executorService.execute(new RunnableSGD(params[x], splittedList.get(i), X_indices, 96, splittedList.get(i).getVec(0).elements.length));
             }
 
             executorService.shutdown();
